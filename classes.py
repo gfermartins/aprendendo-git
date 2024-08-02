0a8ac16e0daa222f3d8a93fc58d15554b456ff01
gfermartins <gabriel.fernandes.martins01@gmail.com>
2024-08-02 13:48:39 -0300

    git

--- classes.py
+++ classes.py
@@ -5,13 +5,13 @@ class Pessoas:
         self.endereco = endereco
 
 class Medico(Pessoas):
-    def __init__(self, nome, sobrenome, endereco, crm, especialidade):
+    def __init__(super, crm, especialidade):
         super().__init__(nome, sobrenome, endereco)
         self.crm = crm
         self.especialidade = especialidade
         self.nome = f"Dr. {self.nome}"  # Prefixar "Dr." ao nome
 
 class Paciente(Pessoas):
-    def __init__(self, nome, sobrenome, endereco, telefone):
+    def __init__(super, telefone):
         super().__init__(nome, sobrenome, endereco)
         self.telefone = telefone
